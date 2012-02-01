b0ccd5093f9e2d60633c383475c40acfe1be572e
Shakthi Prasad <shakthi.gs87@gmail.com>
2012-02-01 14:22:17 +0530

    23 errors more

--- test.cpp
+++ test.cpp
@@ -50,7 +50,7 @@ struct tableForiegn:tableForiegnBase
 
 
 
-template <typename _primaryKeyType,typename _foreignKeytuple>
+template <typename _primaryKeyType,typename _foreignKeytuple=void>
 struct tableTuple {
 	
 	
@@ -176,8 +176,47 @@ struct tableTuple {
 
 
 
+template <typename _primaryKeyType>
+struct tableTuple <_primaryKeyType,void>{
+	
+	
+	typedef _primaryKeyType primaryKeyType;
+	
+	primaryKeyType primaryKey;
+	
+	bool operator < (const tableTuple & u)const
+	{
+		return primaryKey < u.primaryKey;
+	}
+	
+	
+	tableTuple(primaryKeyType k)
+	:primaryKey(k)
+	{
+		
+	}
+	
+	
+	bool validate()
+	{
+		return true;
+	}
+	
+	static bool validateForeignKeyType()
+	{
+		return true;
+	}
+	
+	
+	
+	
+};
 
-template <typename _tableTupleType,typename __foreignTablesType>
+
+
+
+
+template <typename _tableTupleType,typename __foreignTablesType=void>
 struct table:map<typename _tableTupleType::primaryKeyType,_tableTupleType> ,tableReferedBy,tableForiegn<_tableTupleType>
 {
 	
@@ -415,21 +454,257 @@ struct table:map<typename _tableTupleType::primaryKeyType,_tableTupleType> ,tabl
 
 	
 };
+
+
+
+template <typename _tableTupleType>
+struct table<_tableTupleType,void>:map<typename _tableTupleType::primaryKeyType,_tableTupleType> ,tableReferedBy,tableForiegn<_tableTupleType>
+{
+	
+	typedef _tableTupleType tableTupleType;
+	
+	
+	typedef table<tableTupleType> selfType;
+	typedef typename map<typename tableTupleType::primaryKeyType,tableTupleType>::iterator iterator;
+	typedef map<typename tableTupleType::primaryKeyType,tableTupleType> mapType;
+	
+	typedef typename tableTupleType::primaryKeyType primaryKeyType;	
+	template<typename _foreignTablesType,size_t index>
+	struct setForeignTableReferenceHelper;
+	
+	template<int index>
+	struct foreignKeyAt 
+	{
+		typedef typename tuple_element<index,typename tableTupleType::foreignKeysType>::type type;
+		static type getForeignKey(iterator i)
+		{
+			const tableTupleType & tableTuple = i->second;
+			return get<index>(tableTuple.foreignKeys);
+		}
+	};
+	
+	template<typename _foreignTablesType>
+	struct setForeignTableReferenceHelper<_foreignTablesType,0> 
+	{
+		static void setForeignTableReference(_foreignTablesType intupleforiegn,selfType * inself)
+		{
+		}
+		
+	};
+	
+	
+	
+	template<typename _foreignTablesType,size_t i>
+	struct setForeignTableReferenceHelper 
+	{
+		static void setForeignTableReference(_foreignTablesType intupleforiegn,selfType * inself)
+		{
+			typename std::tr1::tuple_element<i-1,_foreignTablesType>::type foreignTable = get<i-1>(inself->foreignTables);
+			foreignTable->referedTables.insert(inself);
+			setForeignTableReferenceHelper<_foreignTablesType,i-1>::setForeignTableReference(intupleforiegn,inself);
+		}
+		
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	table()
+	{
+		tableTupleType::validateForeignKeyType();
+		
+		
+	}
+	
+	iterator findTuple(primaryKeyType key)
+	{
+		
+#if DEBUG		
+		for(iterator i =mapType::begin();  i !=mapType::end(); i++)
+		{
+			tableTupleType rowss=i->second;
+			
+			if(key == i->first)
+				return i;
+			
+			
+		}
+#endif		
+		
+		
+		return mapType::find(key);
+	}
+	
+	
+	
+	bool contains(primaryKeyType key)
+	{
+		return findTuple(key) != mapType::end();
+	}
+	
+	
+	
+	
+	
+	
+	
+	template<class _foreignTableType,typename _foreignKeyType>
+	bool  OnDelete(_foreignKeyType fk,const _foreignTableType &treftable,iterator i)
+	{
+		mapType::erase (i);
+		return true;
+	} 
+	
+	
+	
+	
+	template<typename _foreignTablesType,int index>
+	struct OnDeleteHelper 
+	{
+		
+		static bool  OnDelete(tableForiegnBase * inForeignTable,void * foreignTableKeyPtr,_foreignTablesType  foreignTables,selfType * _self)
+		{
+			typedef typename tuple_element<index-1,_foreignTablesType>::type foreignTableType;
+			
+			tableForiegnBase*  foreignTable = static_cast<tableForiegnBase*>(get<index-1>(foreignTables));
+			
+			
+			
+			if(foreignTable == inForeignTable)
+			{
+				
+				for(iterator i =_self->mapType::begin();  i !=_self->mapType::end(); i++)
+				{
+					
+					
+					typedef typename foreignKeyAt<index-1>::type foreignKeyType;
+					
+					foreignKeyType foreignKey = foreignKeyAt<index-1>::getForeignKey(i);
+					
+					foreignKeyType foreignTableKey = *(static_cast<foreignKeyType*>(foreignTableKeyPtr)); 
+					
+					if(foreignTableKey == foreignKey)
+					{
+						
+						if(_self->OnDelete(foreignTableKey,get<index-1>(foreignTables),i) ==false)
+							return false;
+						
+						break;
+					}
+					
+				}	
+				
+				
+				
+			}
+			
+			
+			
+			
+			return OnDeleteHelper<foreignTablesType,index-1 >::OnDelete(foreignTable,foreignTableKeyPtr,foreignTables,_self);
+			
+			
+		}
+		
+	};
+	
+	
+	
+	
+	
+	template<typename _foreignTablesType>
+	struct OnDeleteHelper<_foreignTablesType,0> 
+	{
+		
+		static bool  OnDelete(tableForiegnBase * inForeignTable,void * foreignTableKeyPtr,_foreignTablesType  foreignTables,selfType * _self)
+		{
+			return true;
+		}
+		
+	};
+	
+	
+	
+	virtual bool OnDelete(tableForiegnBase * inForeignTable,void * inForeignKeyPtr)
+	{
+		const size_t size=  tuple_size<foreignTablesType>::value;
+		return OnDeleteHelper<foreignTablesType,size >::OnDelete(inForeignTable,inForeignKeyPtr,foreignTables,this);
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	bool insert(tableTupleType tableTuple)
+	{
+		
+		if(tableTuple.validate() == false)
+			return false;
+		
+		
+		pair<iterator,bool> a = mapType::insert (make_pair(tableTuple.primaryKey, tableTuple));
+		
+		if(a.second==false)
+			return false;
+		
+		
+		if(tableTuple.validateForeignKey(foreignTables) == false)
+		{
+			mapType::erase(tableTuple.primaryKey);
+			return false;
+		}	
+		
+		return true;
+	}
+	
+	
+	
+	bool deleteKey(typename tableTupleType::primaryKeyType key)
+	{
+		
+		assert(contains(key));
+		
+		typedef typename std::set<tableReferedBy * >::iterator referediter;
+		
+		for(referediter i= tableForiegn<tableTupleType>::referedTables.begin(); i!= tableForiegn<tableTupleType>::referedTables.end(); i++)
+		{
+			if((*i)->OnDelete(this,&key) == false)
+				return false;
+		}
+		
+		iterator k=findTuple(key);
+		if(k!=mapType::end())
+			mapType::erase (k);
+		
+		return true;
+	}
+	
+	
+};
+
+
 #define SHOULD_PASS(x) assert((x) );
 #define SHOULD_FAIL(x) assert(!(x) );
 
 
 
-typedef tuple<> tableTuplei;
 
-struct Player2:tableTuple<int,tableTuplei > {
+struct Player:tableTuple<int> {
 	int playerid()
 	{
 		return primaryKey;
 	}
 	
-	Player2(int playerid,string name)
-	:tableTuple<int,tableTuplei>(playerid,make_tuple()),name(name)
+	Player(int playerid,string name)
+	:tableTuple<int>(playerid),name(name)
 	{}
 	string name;
 };
@@ -455,7 +730,7 @@ struct Score2:tableTuple<int,tuple<int> > {
 int main()
 {
 
-	table<Player2,tuple<> > b(make_tuple());
+	table<Player> b();
 	
 	
 	table<Score2,tuple<tableForiegn<Player2>* > > s(make_tuple(&b));
